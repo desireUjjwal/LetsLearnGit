a8cba06585a0ff8f3d298fec9e7e662e08bc1ba9
Ujjwal <discoverujjwal@gmail.com>
2023-04-29 17:35:39 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+package LetsLearnGit;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Multiplication is: "+(a*b));
+    }
+}
